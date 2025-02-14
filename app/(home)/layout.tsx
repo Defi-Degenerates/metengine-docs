8497e4b8e62b9a1d2e237800943508a16e28e668
Shubham Patel <shubhamashishpatel@gmail.com>
2025-02-14 21:48:55 +0530

    added favicon

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,17 @@
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
+import type { Metadata } from "next";
 import { source } from '@/lib/source';
+
+export const metadata: Metadata = {
+  title: "MetEngine",
+  description: "One - click LP, Copy trade DLMM positions, track IL in Real-Time all from Telegram.",
+  icons: {
+    icon: "/favicon.jpeg",
+  },
+};
+
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
